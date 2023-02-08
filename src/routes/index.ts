21b242ebdc849ddb55181c86e2bc1d92a94f5fbf
aliceb91 <alice.birkett@outlook.com>
2023-02-08 09:51:28 +0000

    removed birds home page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,8 @@
 import express from 'express';
-import homePage from './homepage/homepage'
 import getMoves from './get-moves/get-moves'
 import getCharacters from './get-characters/get-characters'
 const router = express.Router();
 
-router.get('/', homePage);
 router.get('/moves/:character', getMoves)
 router.get('/charList', getCharacters)
 
